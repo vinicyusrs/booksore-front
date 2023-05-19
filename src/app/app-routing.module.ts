4bab78bb7187fbc15fe28edb9d0398d52507c021
vinicyusrs <vinicyus.rs@gmail.com>
2023-05-19 18:02:51 -0300

    update para categoria foi implementado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './components/views/home/home.component';
 import { CategoriaReadComponent } from './components/views/categoria/categoria-read/categoria-read.component';
 import { CategoriaCreateComponent } from './components/views/categoria/categoria-create/categoria-create.component';
 import { CategoriaDeleteComponent } from './components/views/categoria/categoria-delete/categoria-delete.component';
+import { CategoriaUpdateComponent } from './components/views/categoria/categoria-update/categoria-update.component';
 
 const routes: Routes = [
   {
@@ -22,7 +23,12 @@ const routes: Routes = [
   {
     path: 'categorias/delete/:id',
     component: CategoriaDeleteComponent
-  }
+  },
+  {
+    path: 'categorias/update/:id',
+    component: CategoriaUpdateComponent
+  },
+ 
 ];
 
 @NgModule({
